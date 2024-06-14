5e0817a8b0b387dc0881ec91cb58d38605ed1efd
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-06-14 15:58:37 +0200

    feat: Scaffolding,+ migration table + seeder

---
+++ database/migrations/2024_06_14_134057_create_comics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->decimal('price', 5, 2); // 5 sono le cifre tot e 2 sono le cifre decimali, e quindi 999,99
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comics');
+    }
+};
